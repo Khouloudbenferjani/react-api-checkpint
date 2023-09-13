f9df4c536b77761196b69ffa812a74601730217e
[200~git config --global user.name FIRST_NAME <ferjanikhouloud@gmail.com>
2023-09-13 17:03:36 +0200

    ract-app-checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+ const [users,setUsers]= useState([])
+
+//  uplod users from data base 
+ const getUsers = async()=>{
+  const response=await axios.get ('http://jsonplaceholder.typicode.com/users')
+console.log (response.data)
+setUsers(response.data)
+
+ }
+
+useEffect (()=>{
+  getUsers()
+
+},[])
+return(
+  <div className="App">
+  {users.map((user,index)=><li key={index}>{user.name}</li>)}
+
+  </div>
+);
 }
 
-export default App;
+
+
+export default App
